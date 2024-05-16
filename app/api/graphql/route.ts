b5bc8b672ce8f9ba709519ce70ad953bd78bde05
jedemarco1030 <jedemarco1030@yahoo.com>
2024-05-16 15:51:12 -0400

    Removed route

--- app/api/graphql/route.ts
+++
@@ -1,7 +0,0 @@
-// import { ApolloServer } from '@apollo/server';
-// import { startServerAndCreateNextHandler } from '@as-integrations/next';
-// import type { NextRequest } from 'next/server';
-//
-// import { prisma } from '@/lib/prisma-client';
-
-// TODO: Add GraphQL logic
